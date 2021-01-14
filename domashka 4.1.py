b7b1554d987d2bd097ca280b68c4842a19f83b41
hhux <inspector946021>
2021-01-14 19:10:12 +0700

    Задачи Вебинара №4

--- domashka 4.1.py
+++ domashka 4.1.py
@@ -1,15 +1,27 @@
 from datetime import datetime, timedelta
+import calendar
 
-d = '2012.12.12'
-n = -5
+d = '2012.01.31'
+n = 1
 my_date = datetime.strptime(d, '%Y.%m.%d')
 
+# Сколько дней в месяце
+all_days = calendar.monthrange(my_date.year, my_date.month)[1]
 
-if my_date.month == 12 and n > 0: #Проверяю, нужно ли добавить +1 к году
-    change_month = my_date.replace(month=n, year=my_date.year+1)
-elif n <0: #Проверяю, нужно ли вычитать месяц
+# Проверяю, нужно ли добавить год
+if my_date.month + n > 12:
+    change_month = my_date.replace(month=n, year=my_date.year + 1)
+
+# Проверяю на январь
+elif my_date.month == 1:
+    change_month = my_date + timedelta(days=all_days)
+
+# Проверяю, нужно ли вычитать месяц
+elif n < 0:
     change_month = my_date.replace(month=my_date.month - (abs(n)))
-else: #Во всех остальный случаях просто прибавляю месяц
+
+# Во всех остальный случаях просто прибавляю месяц
+else:
     change_month = my_date.replace(month=n)
 
 print(change_month)
